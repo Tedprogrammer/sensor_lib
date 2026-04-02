ac63231a6df52ed4a7bb3c48c8be25eb18c4fed0
Ted Trinh <luonghieutrinh@gmail.com>
2026-04-02 19:17:01 +1100

    Restore SimpleSensor source files in src

--- src/SimpleSensor.h
+++ src/SimpleSensor.h
@@ -1,1 +1,22 @@
+#ifndef SIMPLESENSOR_H
+#define SIMPLESENSOR_H
 
+#include <Arduino.h>
+
+class SimpleSensor {
+  private:
+    int _pin;
+    int _rawValue;
+    int _minCal;
+    int _maxCal;
+
+  public:
+    SimpleSensor(int pin);
+    void begin();
+    void update();
+    int getRaw();
+    void setCalibration(int minCal, int maxCal);
+    int getMappedValue(int outMin, int outMax);
+};
+
+#endif
